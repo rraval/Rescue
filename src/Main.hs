7d04be7791d2e19f4028d18a7e06f9ee366b535e
Ronuk Raval <ronuk.raval@gmail.com>
2010-05-23 20:10:34 -0400

    LaTeX template almost complete.

---
+++ src/Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Rescue.Latex
+
+resume = do
+  preamble "John Doe"
+           "1234 Fifth Street, Province, Country"
+           "(123) 456-7890"
+           "example@example.com"
+  skills
+  education
+  postscript
+
+skills = section "Skills" $ do
+           item "I like to do stuff"
+           item "I'm also really good at other stuff"
+
+education = section "Education" $ do
+              bigitem "University of Someplace"
+                      (date "Someplace, Country (From Date -- To Date)")
+                      (newline $ unlines ["Candidate for a degree",
+                                          "In some program at some faculty or other"])
+              subsection "Relevant Courses and Assignments" $ do
+                      eduitem "Course Title"
+                              "Location"
+                              (date "From Date -- To Date")
+                              "Description of what tasks were completed and why they are relevant."
+                      eduitem "Some other Course"
+                              "At the end of the world"
+                              (date "0 AD -- 0 AD")
+                              "Insert filler material."
+
+main = putStrLn $ show resume
--- src/Rescue/Latex.hs
+++ src/Rescue/Latex.hs
@@ -12,6 +12,11 @@ module Rescue.Latex where
     instance Show (Resume a) where
         show (Resume a) = a ""
 
+    date :: String -> String
+    date = id
+
+    newline :: String -> String
+    newline = id
 
     surround :: String -> String -> Resume a -> Resume a
     surround start end resume = do
@@ -21,7 +26,132 @@ module Rescue.Latex where
                       
     section :: String -> Resume a -> Resume a
     section s = surround ("\\begin{ressection}{" ++ s ++ "}\n")
-                         "\\end{ressection}"
+                         "\\end{ressection}\n"
+
+    subsection :: String -> Resume a -> Resume a
+    subsection s = surround ("\\begin{reslist}{" ++ s ++ "}\n")
+                            "\\end{reslist}\n"
 
     item :: String -> Resume a
     item s = Resume $ \n -> "\\resitem{" ++ s ++ "}\n" ++ n
+
+    bigitem :: String -> String -> String -> Resume a
+    bigitem loc time desc = Resume $ \n ->
+      "\\resbigitem{" ++ loc ++ "}{" ++ time ++ "}{" ++ desc ++ "}\n" ++ n
+
+    eduitem :: String -> String -> String -> String -> Resume a
+    eduitem title loc time desc = Resume $ \n ->
+      "\\eduitem{" ++ title ++ "}{" ++ loc ++ "}{" ++ time ++ "}{" ++ desc ++ "}\n" ++ n
+
+    postscript :: Resume a
+    postscript = Resume $ ("\\end{document}\n"++)
+                      
+    preamble :: String -> String -> String -> String -> Resume a
+    preamble name addr phone email = Resume $ \n -> unlines [
+      "\\documentclass[11pt,oneside]{article}",
+      "\\usepackage{geometry}",
+      "\\usepackage[T1]{fontenc}",
+      "\\usepackage{hyperref}",
+
+      "\\pagestyle{empty}",
+      "\\geometry{letterpaper,tmargin=1in,bmargin=1in,lmargin=1in,rmargin=1in,",
+      "    headheight=0in,headsep=0in,footskip=.3in}",
+
+      "\\setlength{\\parindent}{0in}",
+      "\\setlength{\\parskip}{0in}",
+      "\\setlength{\\itemsep}{0in}",
+      "\\setlength{\\topsep}{0in}",
+      "\\setlength{\\tabcolsep}{0in}",
+
+      "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
+      "% New commands and environments",
+
+      "% This defines how the name looks",
+      "\\newcommand{\\bigname}[1]{",
+      "    \\begin{center}\\fontfamily{phv}\\selectfont\\Huge\\scshape#1\\end{center}",
+      "}",
+
+      "\\newcommand{\\respara}[1]{",
+      "    \\vspace{4pt}",
+      "    {\\fontfamily{phv} \\selectfont \\Large #1} \\\\",
+      "    \\vspace{4pt}",
+      "    \\hspace{19pt}",
+      "}",
+
+      "% A ressection is a main section (<H1>Section</H1>)",
+      "\\newenvironment{ressection}[1]{",
+      "    \\respara{#1}",
+      "    \\begin{itemize}",
+      "    \\vspace{-20pt}",
+      "}{",
+      "    \\end{itemize}",
+      "}",
+
+      "% A resitem is a simple list element in a ressection (first level)",
+      "\\newcommand{\\resitem}[1]{",
+      "    \\vspace{-4pt}",
+      "    \\item \\begin{flushleft} #1 \\end{flushleft}",
+      "}",
+
+      "\\newcommand{\\resrightitem}[1]{",
+      "    \\vspace{-4pt}",
+      "    \\item \\begin{flushright} #1 \\end{flushright}",
+      "}",
+
+      "% A ressubitem is a simple list element in anything but a ressection (second level)",
+      "\\newcommand{\\ressubitem}[1]{",
+      "    \\vspace{-1pt}",
+      "    \\item \\begin{flushleft} #1 \\end{flushleft}",
+      "}",
+
+      "% A resbigitem is a complex list element for stuff like jobs and education:",
+      "%  Arg 1: Name of company or university",
+      "%  Arg 2: Location",
+      "%  Arg 3: Title and/or date range",
+      "\\newcommand{\\resbigitem}[3]{",
+      "    %\\vspace{-5pt}",
+      "    \\item",
+      "    \\textbf{#1} \\hfill \\textit{#2} \\\\",
+      "    #3",
+      "}",
+
+      "\\newcommand{\\eduitem}[4]{",
+      "  \\item",
+      "    \\textbf{#1} \\hfill \\textit{#2} \\\\ \\mbox{} \\hfill \\textit{#3} \\\\",
+      "    #4",
+      "}",
+
+      "% This is a list that comes with a resbigitem",
+      "\\newenvironment{ressubsec}[3]{",
+      "    \\resbigitem{#1}{#2}{#3}",
+      "    \\vspace{-2pt}",
+      "    \\begin{itemize}",
+      "    \\vspace{-1.7em}",
+      "}{",
+      "    \\end{itemize}",
+      "}",
+
+      "% This is a simple sublist",
+      "\\newenvironment{reslist}[1]{",
+      "    \\resitem{\\textbf{#1}}",
+      "    \\vspace{-5pt}",
+      "    \\begin{itemize}",
+      "}{",
+      "    \\end{itemize}",
+      "}",
+
+
+
+      "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
+      "% Now for the actual document:",
+
+      "\\begin{document}",
+
+      "\\fontfamily{ppl} \\selectfont",
+
+      "% Name with horizontal rule",
+      "\\bigname{" ++ name ++ "}",
+      "\\vspace{-8pt} \\rule{\\textwidth}{1pt}",
+      "\\vspace{-1pt} {\\small\\itshape " ++ addr ++ " \\hfill "
+          ++ phone ++ "; " ++ email ++ "}",
+      "\\vspace{8 pt}\n\n"] ++ n
